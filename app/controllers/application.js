ca0eb6cdbfc8c583d34d3898899969b4ad8d8a33
Harry Steinke <Harry Steinke>
2017-07-05 18:46:13 -0500

    ready for challenges

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,12 +5,26 @@ export default Ember.Controller.extend({
   greeting: `hey"'"''"' there`,
   title: 'chef',
   foods: [1, 2, 3, 4, 5, 6],
+  chefsOnStaff: Ember.computed.alias('model.length'),
+  chefsOnSite: Ember.computed.filterBy('model', 'isAvailable', true),
   actions: {
     enter(chef) {
-        Ember.set(chef, 'isAvailable', true)
-      },
-      exit(chef) {
-        Ember.set(chef, 'isAvailable', false)
-      }
+      Ember.set(chef, 'isAvailable', true)
+      chef.save();
+    },
+    exit(chef) {
+      Ember.set(chef, 'isAvailable', false)
+      chef.save();
+    },
+    addNewChef() {
+      this.store.createRecord('chef', {
+          isAvailable: false,
+          name: this.get('newChef')
+        }).save(),
+        this.set('newChef', '')
+    },
+    removeChef(chef){
+      chef.destroyRecord()
+    }
   }
 });
